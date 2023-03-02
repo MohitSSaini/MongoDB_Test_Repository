7466f0db2c12319db32992a9a16a85deab43147d
Mohit__Saini <mohit.saini09868@gmail.com>
2023-03-02 12:30:57 +0530

    added findByProductName method for CustomerRepository

--- src/main/java/com/example/mongodb_test_repository/repository/CustomerRepository.java
+++ src/main/java/com/example/mongodb_test_repository/repository/CustomerRepository.java
@@ -8,5 +8,9 @@ package com.example.mongodb_test_repository.repository;
 import com.example.mongodb_test_repository.domain.Customer;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends MongoRepository<Customer, Long> {
+
+    Optional<Customer> findByName(String name);
 }
