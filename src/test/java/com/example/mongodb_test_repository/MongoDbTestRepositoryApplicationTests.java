49e358ce8a25d66741cbd49258b231547dad8131
Mohit__Saini <mohit.saini09868@gmail.com>
2023-03-02 12:16:22 +0530

    initial commit

---
+++ src/test/java/com/example/mongodb_test_repository/MongoDbTestRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongodb_test_repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbTestRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
