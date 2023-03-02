94edde5713462ed584819e72cdeef98780e755f3
Mohit__Saini <mohit.saini09868@gmail.com>
2023-03-02 12:21:56 +0530

    added fields for Product

---
+++ src/main/java/com/example/mongodb_test_repository/domain/Product.java
@@ -0,0 +1,15 @@
+/*
+ * Author Name: Mohit Saini
+ * Date: 02-03-2023
+ * Created With: IntelliJ IDEA Ultimate
+ */
+package com.example.mongodb_test_repository.domain;
+
+public class Product {
+
+    private long id;
+
+    private String name;
+
+    private String description;
+}
