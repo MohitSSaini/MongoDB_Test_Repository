6cef0ab005f378e7076ab3d0059842cd8985ed4c
Mohit__Saini <mohit.saini09868@gmail.com>
2023-03-02 12:23:09 +0530

    added toString method for Product

--- src/main/java/com/example/mongodb_test_repository/domain/Product.java
+++ src/main/java/com/example/mongodb_test_repository/domain/Product.java
@@ -60,4 +60,13 @@ public class Product {
     public int hashCode() {
         return Objects.hash(id, name, description);
     }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
