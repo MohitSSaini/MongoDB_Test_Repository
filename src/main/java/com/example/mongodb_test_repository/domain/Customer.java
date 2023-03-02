efd2eb4a4c57e13f6a97952201d7b67aca210244
Mohit__Saini <mohit.saini09868@gmail.com>
2023-03-02 12:25:18 +0530

    added fields for Customer

--- src/main/java/com/example/mongodb_test_repository/domain/Customer.java
+++ src/main/java/com/example/mongodb_test_repository/domain/Customer.java
@@ -5,9 +5,18 @@
  */
 package com.example.mongodb_test_repository.domain;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 public class Customer {
+    @Id
+    private long id;
+
+    private String name;
+
+    private String phoneNo;
+
+    private Product product;
 
 }
